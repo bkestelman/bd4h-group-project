932ea9800e71b967257cbebad39c9654788b1fee
Benito Kestelman <bkestelman3@gatech.edu>
2020-04-01 23:35:39 +0000

    fix bugs

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -2,8 +2,8 @@ chmod 400 key.pem
 sudo yum install -y pssh     # Install parallel ssh client
 pscp.pssh -h cluster_hosts.conf -x "-oStrictHostKeyChecking=no -i key.pem" requirements.txt ~
 pssh -i -h cluster_hosts.conf -x "-oStrictHostKeyChecking=no -i key.pem" \
-    "sudo mkdir ~/nltk_data; \
-    sudo chmod 777 ~/nltk_data; \
+    "sudo mkdir /home/nltk_data; \
+    sudo chmod 777 /home/nltk_data; \
     sudo pip install -U -r requirements.txt"
 sudo pip install -U -r requirements.txt
 
