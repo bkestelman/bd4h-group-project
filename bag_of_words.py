ca57aae90f371c6b67e59250e89fe3c394c5c58f
Benito Kestelman <bkestelman3@gatech.edu>
2020-04-02 19:23:12 +0000

    Use Spark's Tokenizer and CountVectorizer for bag-of-words

---
+++ bag_of_words.py
@@ -0,0 +1,17 @@
+import nltk
+from pyspark.sql.functions import col, when, udf
+from pyspark.ml.feature import CountVectorizer, Tokenizer, RegexTokenizer, StopWordsRemover
+
+import nltk_tokenizer
+
+def tokenize(df, text_col):
+    tokenizer = RegexTokenizer(inputCol=text_col, outputCol='RAW_TOKENS', pattern='\\W')
+    result = tokenizer.transform(df)
+    remover = StopWordsRemover(inputCol='RAW_TOKENS', outputCol=text_col+'_TOKENIZED')
+    return remover.transform(result)
+
+def add_bag_of_words(df, words_col):
+    cv = CountVectorizer(inputCol=words_col, outputCol='FEATURES')
+    model = cv.fit(df)
+    result = model.transform(df)
+    return result
