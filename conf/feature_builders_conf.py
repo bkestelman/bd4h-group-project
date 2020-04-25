486995b21b5b00e225a61ca2f11b02ab7f2901e6
Benito Kestelman <bkestelman3@gatech.edu>
2020-04-25 17:50:59 +0000

    Add comment describing conf change

--- conf/feature_builders_conf.py
+++ conf/feature_builders_conf.py
@@ -1,3 +1,9 @@
+# Different pipelines now have the option to choose a specific column as input
+# Currently the choice is between using the raw text (actually almost raw - has 
+# numbers removed; see etl.py) and using further cleaned and spell checked 
+# tokenized text. 
+# 'TEXT' (raw text) is the default
+# 'TOKENS' is the cleaned and tokenized text
 pipeline_input_cols = {
     'BasicWord2Vec': 'TOKENS'
 }
